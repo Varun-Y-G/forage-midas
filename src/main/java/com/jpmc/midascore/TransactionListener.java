4d665ede8006b0e47e8a459d737d7184ad376906
Varun Y G <varunyg777@gmail.com>
2026-04-16 11:44:29 +0530

    Configure H2 database and implement JPA-based transaction persistence layer

---
+++ src/main/java/com/jpmc/midascore/TransactionListener.java
@@ -0,0 +1,26 @@
+package com.jpmc.midascore;
+
+import com.jpmc.midascore.component.DatabaseConduit;
+import com.jpmc.midascore.foundation.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionListener.class);
+    private final DatabaseConduit databaseConduit;
+
+    public TransactionListener(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
+
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-group")
+    public void listen(Transaction transaction) {
+        logger.info("Received transaction: {}", transaction);
+        databaseConduit.processTransaction(transaction);
+     
+    }
+}
